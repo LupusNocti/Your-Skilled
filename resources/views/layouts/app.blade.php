f630820c5a3bdae772ced16fbf6c03b30815d1d7
LupusNocti <vinceqcaballero@gmail.com>
2024-01-11 22:56:25 +0800

    Your Skilled Filipino

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Your head content here -->
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <!-- Your scripts here -->
+</body>
+</html>
